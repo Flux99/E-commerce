6c002abcc892494ce84bebdd63209715f826b8d7
Flux99 <9901anonymous@gmail.com>
2020-10-21 22:13:06 +0530

    deleted extra files

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -8,6 +8,8 @@ import {PaymentCreatedListener} from "./events/listeners/payment-created-listene
 
 
 const start = async () => {
+  console.log("Starting...")
+
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
