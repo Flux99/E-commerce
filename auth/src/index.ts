52d70125e05d7eabd409f83c2c5ad29ecada5f77
Flux99 <9901anonymous@gmail.com>
2020-10-21 20:04:37 +0530

    added startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,7 +4,7 @@ import { app } from './app';
 
 const start = async () => {
   
-  console.log("Starting Up...")
+  console.log("Starting Up....")
 
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
